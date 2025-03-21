0e1d1b55fbd5220258fea171c7abb7f7ca961bc3
Grzegorz Bielski <pesiok@gmail.com>
2025-03-21 22:43:55 +0100

    fix semiauto specs

---
+++ generic-extras/src/test/scala-3/io/circe/generic/extras/ConfiguredSemiautoDerivedSuite.scala
@@ -0,0 +1,186 @@
+/*
+ * Copyright 2019 circe
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.circe.generic.extras
+
+import cats.kernel.Eq
+import io.circe.Codec
+import io.circe.Decoder
+import io.circe.DecodingFailure
+import io.circe.Encoder
+import io.circe.Json
+import io.circe.generic.extras.semiauto.*
+import io.circe.literal.*
+import io.circe.syntax.*
+import io.circe.testing.CodecTests
+import org.scalacheck.Arbitrary
+import org.scalacheck.Arbitrary.arbitrary
+import org.scalacheck.Gen
+import org.scalacheck.Prop.forAll
+
+import examples.*
+
+object ConfiguredSemiautoDerivedSuite {
+  sealed trait ConfigExampleBase
+  case class ConfigExampleFoo(thisIsAField: String, a: Int = 0, b: Double) extends ConfigExampleBase
+  case object ConfigExampleBar extends ConfigExampleBase
+
+  object ConfigExampleFoo {
+    implicit val eqConfigExampleFoo: Eq[ConfigExampleFoo] = Eq.fromUniversalEquals
+    val genConfigExampleFoo: Gen[ConfigExampleFoo] = for {
+      thisIsAField <- arbitrary[String]
+      a <- arbitrary[Int]
+      b <- arbitrary[Double]
+    } yield ConfigExampleFoo(thisIsAField, a, b)
+    implicit val arbitraryConfigExampleFoo: Arbitrary[ConfigExampleFoo] = Arbitrary(genConfigExampleFoo)
+  }
+
+  object ConfigExampleBase {
+    implicit val eqConfigExampleBase: Eq[ConfigExampleBase] = Eq.fromUniversalEquals
+    val genConfigExampleBase: Gen[ConfigExampleBase] =
+      Gen.oneOf(Gen.const(ConfigExampleBar), ConfigExampleFoo.genConfigExampleFoo)
+    implicit val arbitraryConfigExampleBase: Arbitrary[ConfigExampleBase] = Arbitrary(genConfigExampleBase)
+  }
+
+  implicit val customConfig: Configuration =
+    Configuration.default.withSnakeCaseMemberNames.withDefaults.withDiscriminator("type").withSnakeCaseConstructorNames
+
+  implicit val decodeConfigExampleBase: Decoder[ConfigExampleBase] = deriveConfiguredDecoder
+  implicit val encodeConfigExampleBase: Encoder.AsObject[ConfigExampleBase] = deriveConfiguredEncoder
+  val codecForConfigExampleBase: Codec.AsObject[ConfigExampleBase] = deriveConfiguredCodec
+}
+
+class ConfiguredSemiautoDerivedSuite extends CirceSuite {
+  import ConfiguredSemiautoDerivedSuite._
+
+  checkAll("Codec[ConfigExampleBase]", CodecTests[ConfigExampleBase].codec)
+  checkAll(
+    "Codec[ConfigExampleBase] via Codec",
+    CodecTests[ConfigExampleBase](codecForConfigExampleBase, codecForConfigExampleBase).codec
+  )
+  checkAll(
+    "Codec[ConfigExampleBase] via Decoder and Codec",
+    CodecTests[ConfigExampleBase](implicitly, codecForConfigExampleBase).codec
+  )
+  checkAll(
+    "Codec[ConfigExampleBase] via Encoder and Codec",
+    CodecTests[ConfigExampleBase](codecForConfigExampleBase, implicitly).codec
+  )
+
+  property("Semi-automatic derivation should support configuration") {
+    forAll { (f: String, b: Double) =>
+      val foo: ConfigExampleBase = ConfigExampleFoo(f, 0, b)
+      val json = json"""{ "type": "config_example_foo", "this_is_a_field": $f, "b": $b}"""
+      val expected = json"""{ "type": "config_example_foo", "this_is_a_field": $f, "a": 0, "b": $b}"""
+
+      assert(Encoder[ConfigExampleBase].apply(foo) === expected)
+      assert(Decoder[ConfigExampleBase].decodeJson(json) === Right(foo))
+    }
+  }
+
+  // this fails because `circe-core` Scala 3 derivation calls the `transformName` functions for each encode / decode invocation.
+  // to be fixed in `circe-core` ???
+  // see `memberNameCache`
+
+  // test("it should call field modification times equal to field count") {
+  //   var transformMemberNamesCallCount, transformConstructorCallCount = 0
+  //   implicit val customConfig: Configuration =
+  //     Configuration.default.copy(
+  //       transformMemberNames = v => {
+  //         transformMemberNamesCallCount = transformMemberNamesCallCount + 1
+  //         Configuration.snakeCaseTransformation(v)
+  //       },
+  //       transformConstructorNames = v => {
+  //         transformConstructorCallCount = transformConstructorCallCount + 1
+  //         Configuration.snakeCaseTransformation(v)
+  //       }
+  //     )
+
+  //   val fieldCount = 3
+  //   val decodeConstructorCount = 2
+  //   val encodeConstructorCount = 1
+
+  //   val encoder: Encoder[ConfigExampleBase] = deriveConfiguredEncoder
+  //   val decoder: Decoder[ConfigExampleBase] = deriveConfiguredDecoder
+  //   for {
+  //     _ <- 1 until 100
+  //   } {
+  //     val foo: ConfigExampleBase = ConfigExampleFoo("field_value", 0, 100)
+  //     val encoded = encoder.apply(foo)
+  //     val decoded = decoder.decodeJson(encoded)
+  //     assert(decoded === Right(foo))
+  //     println("transformMemberNamesCallCount" -> transformMemberNamesCallCount)
+  //     println("transformConstructorCallCount" -> transformConstructorCallCount)
+  //     assert(transformMemberNamesCallCount === fieldCount * 2) // failure: 12 in second call
+  //     assert(transformConstructorCallCount === decodeConstructorCount + encodeConstructorCount)
+  //   }
+  // }
+
+  property("it should support configured strict decoding") {
+    forAll { (f: String, b: Double) =>
+      implicit val customConfig: Configuration =
+        Configuration.default.withSnakeCaseMemberNames.withDefaults
+          .withDiscriminator("type_field")
+          .withSnakeCaseConstructorNames
+          .withStrictDecoding
+
+      implicit val decodeConfigExampleBase: Decoder[ConfigExampleBase] = deriveConfiguredDecoder
+
+      val json =
+        json"""
+            {"type_field": "config_example_foo", "this_is_a_field": $f, "b": $b, "stowaway_field": "I should not be here"}
+        """
+
+      val expectedError =
+        DecodingFailure("Strict decoding ConfigExampleFoo - unexpected fields: stowaway_field; valid fields: this_is_a_field, a, b, type_field.", Nil)
+
+
+      assert(Decoder[ConfigExampleBase].decodeJson(json) === Left(expectedError))
+    }
+  }
+
+  // fails
+  property("it should not transform discriminator for strict decoding checks") {
+    forAll { (f: String, b: Double) =>
+      implicit val customConfig: Configuration =
+        Configuration.default.withSnakeCaseMemberNames.withDefaults
+          .withDiscriminator("typeField")
+          .withSnakeCaseConstructorNames
+          .withStrictDecoding
+
+      implicit val decodeConfigExampleBase: Decoder[ConfigExampleBase] = deriveConfiguredDecoder
+
+      val json = json"""{"typeField": "config_example_foo", "this_is_a_field": $f, "b": $b}"""
+      val jsonExtra = json.mapObject(_.add("stowaway_field", "I should not be here".asJson))
+
+      val expectedError =
+        DecodingFailure("Strict decoding ConfigExampleFoo - unexpected fields: stowaway_field; valid fields: this_is_a_field, a, b, typeField.", Nil)
+
+      // We should not transform `typeField` into snake case when checking strictly used fields.
+      assert(clue(Decoder[ConfigExampleBase].decodeJson(json)).isRight)
+      assert(Decoder[ConfigExampleBase].decodeJson(jsonExtra) === Left(expectedError))
+    }
+  }
+
+  property("A generically derived codec for an empty case class should not accept non-objects") {
+    forAll { (j: Json) =>
+      case class EmptyCc()
+
+      assert(deriveConfiguredDecoder[EmptyCc].decodeJson(j).isRight == j.isObject)
+      assert(deriveConfiguredCodec[EmptyCc].decodeJson(j).isRight == j.isObject)
+    }
+  }
+}
---
+++ generic-extras/src/test/scala-3/io/circe/generic/extras/EnumerationSemiautoDerivedSuite.scala
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2019 circe
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.circe.generic.extras
+
+import io.circe.Codec
+import io.circe.Decoder
+import io.circe.Encoder
+import io.circe.generic.extras.semiauto.*
+import io.circe.literal.*
+import io.circe.testing.CodecTests
+
+import examples._
+
+class EnumerationSemiautoDerivedSuite extends CirceSuite {
+  implicit val decodeCardinalDirection: Decoder[CardinalDirection] = deriveEnumerationDecoder
+  implicit val encodeCardinalDirection: Encoder[CardinalDirection] = deriveEnumerationEncoder
+  val codecForCardinalDirection: Codec[CardinalDirection] = deriveEnumerationCodec
+
+  checkAll("Codec[CardinalDirection]", CodecTests[CardinalDirection].codec)
+  checkAll(
+    "Codec[CardinalDirection] via Codec",
+    CodecTests[CardinalDirection](codecForCardinalDirection, codecForCardinalDirection).codec
+  )
+  checkAll(
+    "Codec[CardinalDirection] via Decoder and Codec",
+    CodecTests[CardinalDirection](implicitly, codecForCardinalDirection).codec
+  )
+  checkAll(
+    "Codec[CardinalDirection] via Encoder and Codec",
+    CodecTests[CardinalDirection](codecForCardinalDirection, implicitly).codec
+  )
+
+  test("deriveEnumerationDecoder should not compile on an ADT with case classes") {
+    assert(
+      compileErrors(
+        """
+        implicit val config: Configuration = Configuration.default
+        val _ = config
+        deriveEnumerationDecoder[ExtendedCardinalDirection]
+        """
+      ).nonEmpty
+    )
+  }
+
+  test("it should respect Configuration snake-case") {
+    implicit val config: Configuration = Configuration.default.withSnakeCaseConstructorNames
+    val _ = config
+    val decodeMary = deriveEnumerationDecoder[Mary]
+    val expected = json""""little_lamb""""
+    assert(decodeMary.decodeJson(expected) === Right(LittleLamb))
+  }
+
+  test("deriveEnumerationEncoder should not compile on an ADT with case classes") {
+    assert(
+      compileErrors(
+        """
+        implicit val config: Configuration = Configuration.default
+        val _ = config
+        deriveEnumerationEncoder[ExtendedCardinalDirection]
+        """
+      ).nonEmpty
+    )
+  }
+
+  test("it should respect Configuration kebab-case") {
+    implicit val config: Configuration = Configuration.default.withKebabCaseConstructorNames
+    val _ = config
+    val encodeMary = deriveEnumerationEncoder[Mary]
+    val expected = json""""little-lamb""""
+    assert(encodeMary(LittleLamb) === expected)
+  }
+}
