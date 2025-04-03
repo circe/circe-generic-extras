983b0c608a2cf891de06011117b3d8853bc666ee
Darren Gibson <zarthross+github@gmail.com>
2025-04-03 10:53:17 -0500

    Add test for Semi auto to ensure its does not derive inner types

--- generic-extras/src/test/scala/io/circe/generic/extras/SharedConfiguredSemiautoDerivedSuite.scala
+++ generic-extras/src/test/scala/io/circe/generic/extras/SharedConfiguredSemiautoDerivedSuite.scala
@@ -25,22 +25,24 @@ import io.circe.generic.extras.semiauto._
 import io.circe.literal._
 import io.circe.testing.CodecTests
 import org.scalacheck.Arbitrary
-import org.scalacheck.Arbitrary.arbitrary
 import org.scalacheck.Gen
 import org.scalacheck.Prop.forAll
 
 object SharedConfiguredSemiautoDerivedSuite {
+
+  case class InnerExample(a: Double)
+  object InnerExample {
+    implicit val eqInnerExample: Eq[InnerExample] = Eq.fromUniversalEquals
+    val genInnerExample: Gen[InnerExample] = Gen.resultOf(InnerExample.apply _)
+    implicit val arbitraryInnerExample: Arbitrary[InnerExample] = Arbitrary(genInnerExample)
+  }
   sealed trait ConfigExampleBase
-  case class ConfigExampleFoo(thisIsAField: String, a: Int = 0, b: Double) extends ConfigExampleBase
+  case class ConfigExampleFoo(thisIsAField: String, a: Int = 0, b: InnerExample) extends ConfigExampleBase
   case object ConfigExampleBar extends ConfigExampleBase
 
   object ConfigExampleFoo {
     implicit val eqConfigExampleFoo: Eq[ConfigExampleFoo] = Eq.fromUniversalEquals
-    val genConfigExampleFoo: Gen[ConfigExampleFoo] = for {
-      thisIsAField <- arbitrary[String]
-      a <- arbitrary[Int]
-      b <- arbitrary[Double]
-    } yield ConfigExampleFoo(thisIsAField, a, b)
+    val genConfigExampleFoo: Gen[ConfigExampleFoo] = Gen.resultOf(ConfigExampleFoo.apply _)
     implicit val arbitraryConfigExampleFoo: Arbitrary[ConfigExampleFoo] = Arbitrary(genConfigExampleFoo)
   }
 
@@ -54,45 +56,77 @@ object SharedConfiguredSemiautoDerivedSuite {
   implicit val customConfig: Configuration =
     Configuration.default.withSnakeCaseMemberNames.withDefaults.withDiscriminator("type").withSnakeCaseConstructorNames
 
-  implicit val decodeConfigExampleBase: Decoder[ConfigExampleBase] = deriveConfiguredDecoder
-  implicit val encodeConfigExampleBase: Encoder.AsObject[ConfigExampleBase] = deriveConfiguredEncoder
-  val codecForConfigExampleBase: Codec.AsObject[ConfigExampleBase] = deriveConfiguredCodec
 }
 
 class SharedConfiguredSemiautoDerivedSuite extends CirceSuite {
   import SharedConfiguredSemiautoDerivedSuite._
 
-  checkAll("Codec[ConfigExampleBase]", CodecTests[ConfigExampleBase].codec)
-  checkAll(
-    "Codec[ConfigExampleBase] via Codec",
-    CodecTests[ConfigExampleBase](codecForConfigExampleBase, codecForConfigExampleBase).codec
-  )
-  checkAll(
-    "Codec[ConfigExampleBase] via Decoder and Codec",
-    CodecTests[ConfigExampleBase](implicitly, codecForConfigExampleBase).codec
-  )
-  checkAll(
-    "Codec[ConfigExampleBase] via Encoder and Codec",
-    CodecTests[ConfigExampleBase](codecForConfigExampleBase, implicitly).codec
-  )
-
-  property("Semi-automatic derivation should support configuration") {
-    forAll { (f: String, b: Double) =>
-      val foo: ConfigExampleBase = ConfigExampleFoo(f, 0, b)
-      val json = json"""{ "type": "config_example_foo", "this_is_a_field": $f, "b": $b}"""
-      val expected = json"""{ "type": "config_example_foo", "this_is_a_field": $f, "a": 0, "b": $b}"""
-
-      assert(Encoder[ConfigExampleBase].apply(foo) === expected)
-      assert(Decoder[ConfigExampleBase].decodeJson(json) === Right(foo))
-    }
+  property("Semi-automatic decoder derivation does NOT derive inner types automatically") {
+    val errors = clue(compileErrors("deriveConfiguredDecoder[ConfigExampleBase]"))
+    assert(
+      List(
+        "members don't have codecs of their own", // Scala 2.13, 3
+        "error: Failed to find an instance of Decoder[io.circe.generic.extras.SharedConfiguredSemiautoDerivedSuite.InnerExample]" // Scala 2.12
+      ).exists(errors.contains)
+    )
+  }
+  property("Semi-automatic encoder derivation does NOT derive inner types automatically") {
+    val errors = clue(compileErrors("deriveConfiguredEncoder[ConfigExampleBase]"))
+    assert(
+      List(
+        "members don't have codecs of their own", // Scala 2.13, 3
+        "error: Failed to find an instance of Encoder[io.circe.generic.extras.SharedConfiguredSemiautoDerivedSuite.InnerExample]" // Scala 2.12
+      ).exists(errors.contains)
+    )
+  }
+  property("Semi-automatic codec derivation does NOT derive inner types automatically") {
+    val errors = clue(compileErrors("deriveConfiguredCodec[ConfigExampleBase]"))
+    assert(
+      List(
+        "members don't have codecs of their own", // Scala 2.13, 3
+        "error: Failed to find an instance of Decoder[io.circe.generic.extras.SharedConfiguredSemiautoDerivedSuite.InnerExample]" // Scala 2.12
+      ).exists(errors.contains)
+    )
   }
 
-  property("A generically derived codec for an empty case class should not accept non-objects") {
-    forAll { (j: Json) =>
-      case class EmptyCc()
+  {
+    implicit val innerExample: Codec[InnerExample] = deriveConfiguredCodec
+    implicit val decodeConfigExampleBase: Decoder[ConfigExampleBase] = deriveConfiguredDecoder
+    implicit val encodeConfigExampleBase: Encoder.AsObject[ConfigExampleBase] = deriveConfiguredEncoder
+    val codecForConfigExampleBase: Codec.AsObject[ConfigExampleBase] = deriveConfiguredCodec
 
-      assert(deriveConfiguredDecoder[EmptyCc].decodeJson(j).isRight == j.isObject)
-      assert(deriveConfiguredCodec[EmptyCc].decodeJson(j).isRight == j.isObject)
+    checkAll("Codec[ConfigExampleBase]", CodecTests[ConfigExampleBase].codec)
+    checkAll(
+      "Codec[ConfigExampleBase] via Codec",
+      CodecTests[ConfigExampleBase](codecForConfigExampleBase, codecForConfigExampleBase).codec
+    )
+    checkAll(
+      "Codec[ConfigExampleBase] via Decoder and Codec",
+      CodecTests[ConfigExampleBase](implicitly, codecForConfigExampleBase).codec
+    )
+    checkAll(
+      "Codec[ConfigExampleBase] via Encoder and Codec",
+      CodecTests[ConfigExampleBase](codecForConfigExampleBase, implicitly).codec
+    )
+
+    property("Semi-automatic derivation should support configuration") {
+      forAll { (f: String, b: InnerExample) =>
+        val foo: ConfigExampleBase = ConfigExampleFoo(f, 0, b)
+        val json = json"""{ "type": "config_example_foo", "this_is_a_field": $f, "b": $b}"""
+        val expected = json"""{ "type": "config_example_foo", "this_is_a_field": $f, "a": 0, "b": $b}"""
+
+        assert(Encoder[ConfigExampleBase].apply(foo) === expected)
+        assert(Decoder[ConfigExampleBase].decodeJson(json) === Right(foo))
+      }
+    }
+
+    property("A generically derived codec for an empty case class should not accept non-objects") {
+      forAll { (j: Json) =>
+        case class EmptyCc()
+
+        assert(deriveConfiguredDecoder[EmptyCc].decodeJson(j).isRight == j.isObject)
+        assert(deriveConfiguredCodec[EmptyCc].decodeJson(j).isRight == j.isObject)
+      }
     }
   }
 }
